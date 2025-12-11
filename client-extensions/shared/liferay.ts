fcd596a6a95fc17d71111fa40d0ba33ec823e10c
Nate M King <natemking@gmail.com>
2025-12-11 10:35:55 -0500

    updated Liferay ts file

--- client-extensions/shared/liferay.ts
+++ client-extensions/shared/liferay.ts
@@ -1,4 +1,10 @@
-interface LiferayType {
+declare global {
+	interface Window {
+		Liferay?: LiferayJSGlobalType;
+	}
+}
+
+type LiferayJSGlobalType = {
 	Language: {
 		get: (key: string) => string;
 	};
@@ -25,36 +31,33 @@ interface LiferayType {
 	authToken: string;
 	on: (event: string, callback: (...args: unknown[]) => void) => void;
 	fire: (event: string, data?: unknown) => void;
-}
+};
 
-export const Liferay: LiferayType =
-	(window as any).Liferay ||
-	({
-		Language: {
-			get: (key: string) => key,
-		},
-		OAuth2: {
-			getAuthorizeURL: () => '',
-			getBuiltInRedirectURL: () => '',
-			getIntrospectURL: () => '',
-			getTokenURL: () => '',
-			getUserAgentApplication: (_serviceName: string) => {},
-		},
-		OAuth2Client: {
-			FromParameters: (_options: Record<string, unknown>) => ({}),
-			FromUserAgentApplication: (_id: string) => ({}),
-			fetch: (_url: string, _options?: Record<string, unknown>) => {},
-		},
-		ThemeDisplay: {
-			getCompanyGroupId: () => 20119,
-			getPathThemeImages: () => '',
-			getPortalURL: () => 'http://localhost:8080',
-			getScopeGroupId: () => 20117,
-			getSiteGroupId: () => 20117,
-			isSignedIn: () => false,
-		},
-		authToken: '',
-		on: (_event: string, _callback: (...args: unknown[]) => void) => {},
-		fire: (_event: string, _data?: unknown) => {},
-	} as LiferayType);
-
+export const Liferay: LiferayJSGlobalType = window.Liferay || {
+	Language: {
+		get: (key: string) => key,
+	},
+	OAuth2: {
+		getAuthorizeURL: () => '',
+		getBuiltInRedirectURL: () => '',
+		getIntrospectURL: () => '',
+		getTokenURL: () => '',
+		getUserAgentApplication: (_serviceName: string) => {},
+	},
+	OAuth2Client: {
+		FromParameters: (_options: Record<string, unknown>) => ({}),
+		FromUserAgentApplication: (_id: string) => ({}),
+		fetch: (_url: string, _options?: Record<string, unknown>) => {},
+	},
+	ThemeDisplay: {
+		getCompanyGroupId: () => 20119,
+		getPathThemeImages: () => '',
+		getPortalURL: () => 'http://localhost:8080',
+		getScopeGroupId: () => 20117,
+		getSiteGroupId: () => 20117,
+		isSignedIn: () => false,
+	},
+	authToken: '',
+	on: (_event: string, _callback: (...args: unknown[]) => void) => {},
+	fire: (_event: string, _data?: unknown) => {},
+};
